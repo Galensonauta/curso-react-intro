f26e7065961ac129956fdb5c8b57d6f74226e622
Galenso <galensoserrano1@gmail.com>
2023-05-25 21:04:00 -0300

    clase 5, segundo css(dealles chicos)

--- src/todoItem.js
+++ src/todoItem.js
@@ -4,7 +4,7 @@ function TodoItem(props){
      <li className="Lis">
         <span className={`Icono Icono-check ${props.hecho && "Icono-check--verde"}`}>
        V </span>
-        <p className={`Context ${props.text && "Context--hecho"}`}>{props.text}
+        <p className={`Context ${props.hecho && "Context--hecho"}`}>{props.text}
         </p>
         <span className="Icono Icono-borrar" >
             X</span>
