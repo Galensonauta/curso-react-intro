9e717f53c56cd2ac6fcd9843a68f833b01fa4c82
Galenso <galensoserrano1@gmail.com>
2023-05-26 16:32:16 -0300

    funcionalidad al buscador, escuchando eventos pero en jsx(react)

--- src/search.js
+++ src/search.js
@@ -2,8 +2,11 @@ import "./search.css"
 function Search(){
     return(
     <input 
-    placeholder="Buscar"
-    />     
+    placeholder="Buscar"     
+    onChange={(event)=>{
+        console.log(event.target.value);
+    }}
+    />
     );
     }
 export{Search}  
