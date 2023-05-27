4e502832a5e9594b734aba1e1e3ebd81f7f10fc5
Galenso <galensoserrano1@gmail.com>
2023-05-27 12:38:12 -0300

    clase 13. Uso de localStorage para usar persistencia de datos. Usar stringify y parse ya que local storage solo reconoce strings

--- src/App.js
+++ src/App.js
@@ -6,20 +6,29 @@ import { Lis } from './lis';
 import { TodoItem } from './todoItem';
 import { Creador } from './creador';
 
-const defaultOdos = [
-  {text: "Universidad de Chilecito",
-  hecho: true},
-  {text: "ALURA/ONE",
-  hecho: false},
-  {text: "Maxi programa",
-  hecho: false},
-  {text: "Fullstack Platzi",
-  hecho: false},
-];
+// const objetivosDefault = [
+//   {text: "Universidad de Chilecito",
+//   hecho: true},
+//   {text: "ALURA/ONE",
+//   hecho: false},
+//   {text: "Maxi programa",
+//   hecho: false},
+//   {text: "Fullstack Platzi",
+//   hecho: false},
+// ];
+// localStorage.setItem("Objetivos_Vb", JSON.stringify(objetivosDefault));
 
 function App() {
+  const localStorageObjetivos = localStorage.getItem("Objetivos_Vb");
+  let parsedObjetivos;
+  if (!localStorageObjetivos) {
+     localStorage.setItem("Objetivos_Vb", JSON.stringify([]));
+     parsedObjetivos=[];
+  }else{
+     parsedObjetivos=JSON.parse(localStorageObjetivos);
+  }
   //Estdos hechos y derechos, con sus estados modificadores(los setNombredeEstado)
-const [todos, setTodos]=React.useState(defaultOdos);
+const [todos, setTodos]=React.useState(parsedObjetivos);
 const [searchValue, setSearchValue]=React.useState("");
 //Los de abajo son estados derivados
 const objetivosLogrados= todos.filter(todo=>todo.hecho).length;
@@ -28,19 +37,24 @@ const busqueda = todos.filter(
   (todo)=>{
    return  todo.text.toLowerCase().includes(searchValue.toLowerCase());
   });
+   const salvarObjetivos = (nuevoObjetivo)=>{
+     localStorage.setItem("Objetivos_Vb", JSON.stringify(nuevoObjetivo))
+     setTodos(nuevoObjetivo);
+   }
+
   const check = (text)=>{
     const nuevoObjetivo=[...todos];
     const ObjetivoIndex= nuevoObjetivo.findIndex((todo)=>todo.text==text
     )
     nuevoObjetivo[ObjetivoIndex].hecho=true
-    setTodos(nuevoObjetivo)
+    salvarObjetivos(nuevoObjetivo)
   }
   const borrar = (text)=>{
     const nuevoObjetivo=[...todos];
     const ObjetivoIndex= nuevoObjetivo.findIndex((todo)=>todo.text==text
     );
     nuevoObjetivo.splice(ObjetivoIndex, 1);
-    setTodos(nuevoObjetivo)
+    salvarObjetivos(nuevoObjetivo)
   }
   return (
     <> 
--- src/todoItem.js
+++ src/todoItem.js
@@ -8,21 +8,11 @@ function TodoItem(props){
         hecho={props.hecho}
         check={props.check}
         />
-        {/* <span 
-         className={`Icono Icono-check ${props.hecho && "Icono-check--verde"}`}
-         check={props.check}
-         >
-         
-       V </span> */}
         <p className={`Context ${props.hecho && "Context--hecho"}`}>{props.text}
         </p>
         <BorrarIcon
         borrar={props.borrar}
         />
-        {/* <span className="Icono Icono-borrar" 
-        borrar={props.borrar}
-        >
-            X</span> */}
         </li>
         );
     }
